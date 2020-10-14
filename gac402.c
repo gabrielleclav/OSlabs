ce33fd237513ba5780e54c62b838a096b0ecc6db
Gabrielle Clavell <56766518+gabrielleclav@users.noreply.github.com>
2020-10-14 12:59:48 -0400

    Update gac402.c

--- gac402.c
+++ gac402.c
@@ -14,56 +14,51 @@ int main (int argc, char * argv[], char * envp[]){
     while(1){
 
         char path[]= "/bin/";    //set path at bin
-        char cmd[10];      //user input
-        char fullPath[10];    //full file path
+        char cmd[10];           //user input
+        char fullPath[10];      //full file path
         int child_status;
 
-        printf("lab1> ");                    //print shell prompt
+        printf("lab1> ");                //print shell prompt
 
-        fgets(cmd, 10, stdin);           // do fgets() or scanf
+        fgets(cmd, 10, stdin);           // do fgets() for the command
 
         cmd[strlen(cmd)-1] = '\0';      // get rid of the \n character at the end
 
         printf( "Parent Process %d\n", (int) getpid());
 
-    if (strcmp(cmd, "exit") == 0) {
+    if (strcmp(cmd, "exit") == 0) {         //if the cmd is exit, exit immediately
         exit(0);
     }
 
-    else if (strcmp(cmd, "greet") == 0) {
+    else if (strcmp(cmd, "greet") == 0) {   // if cmd = greet, print hello
         printf("Hello\n");
 
     }
-    else if (strcmp(cmd, "printid") == 0) {
+    else if (strcmp(cmd, "printid") == 0) { // printid, print getpid()
         printf("%d", getpid());
 
      }
     // if the string entered by the user is a built-in command
         // check the string and based on it, do the corresponding tasks
     else {
-        //strcpy(fullPath, path);
-        strcat(fullPath, path);
-        strcat(fullPath, cmd);
-        //fullPath[strlen(fullPath)-1] = '\0';
+
+        strcat(fullPath, path);     //concatenate the path to fullpath
+        strcat(fullPath, cmd);      //concatenate the cmd to fullpath
+
 
         //printf("%s\n", fullPath); // checking to see if it is added properly
 
-        char *argv[] = {cmd, NULL};
+        char *argv[] = {cmd, NULL};     //second arg into execve
 
         pid_t child = fork();    //fork child
 
         if(child == 0) {             //Child
             printf( "Child process %d will execute the command %s\n", (int) getpid(), cmd);
-            // add in
-            //execve - use string entered by user to transform the child process to the
-            // new process inside will be execve(&fullPath, progname, NULL);
 
             // if execution of the child process reaches this point, it means execve failed.
-            // in that case, print "Command Not Found" and exits
-                //return -1; // error
             if (execve(fullPath, argv, NULL)) {
                 printf("Command Not Found\n");
-                exit(-11);
+                exit(-1);
             }
         }
     }
